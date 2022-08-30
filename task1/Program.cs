e81779f898346e34f8fdd765225208249b781f5f
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-08-30 14:20:14 +0300

    Task resolved

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите пятизначное число:");
+string number = Console.ReadLine();
+
+if (number.Length == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("Число палиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число не палиндром");
+    }
+}
+else
+{
+    Console.WriteLine("Число не пятизначное!");
+}
